fc3871150f6a83d98cc32ddfe37061d1761df909
KyongHwan-Kim <dolkys123@sju.ac.kr>
2024-06-09 22:08:38 +0900

    feat: init project

---
+++ spec/src/main/kotlin/com/openlab/openapi_platform/OpenapiPlatformApplication.kt
@@ -0,0 +1,11 @@
+package com.openlab.openapi_platform
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OpenapiPlatformApplication
+
+fun main(args: Array<String>) {
+	runApplication<OpenapiPlatformApplication>(*args)
+}
