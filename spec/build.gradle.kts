e7aa95870faa250d2aee16dc4da6ee5fead6c9ce
KyongHwan-Kim <dolkys123@sju.ac.kr>
2024-06-09 22:42:24 +0900

    add : controller

--- spec/build.gradle.kts
+++ spec/build.gradle.kts
@@ -1,8 +1,11 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.openapitools.generator.gradle.plugin.tasks.GenerateTask
 plugins {
 	id("org.springframework.boot") version "3.3.0"
 	id("io.spring.dependency-management") version "1.1.5"
 	kotlin("jvm") version "1.9.24"
 	kotlin("plugin.spring") version "1.9.24"
+	id("org.openapi.generator") version "6.2.0"
 }
 
 group = "com.openlab"
@@ -22,6 +25,13 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.1.0")
+	implementation("org.openapitools:jackson-databind-nullable:0.2.6")
+	implementation("javax.validation:validation-api:2.0.1.Final")
+	implementation("javax.annotation:javax.annotation-api:1.3.2")
+	implementation("javax.servlet:javax.servlet-api:4.0.1")
+	implementation("org.apache.httpcomponents.client5:httpclient5:5.2.1")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
@@ -35,4 +45,42 @@ kotlin {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+
+// openapi 문서를 Generate하기 위한 설정
+task<GenerateTask>("generateApiDoc") {
+	generatorName.set("html2")
+	inputSpec.set("$projectDir/src/main/resources/openapi/spec-docs.yaml")
+	outputDir.set("$buildDir/openapi/doc/")
+}
+
+task<GenerateTask>("generateApiServer") {
+	generatorName.set("kotlin-spring")
+	inputSpec.set("$projectDir/src/main/resources/openapi/spec-docs.yaml")
+	outputDir.set("$buildDir/openapi/server-code/")
+	apiPackage.set("com.openlab.openapi_platform.openapi.generated.controller")
+	modelPackage.set("com.openlab.openapi_platform.openapi.generated.model")
+	configOptions.set(
+		mapOf(
+			"interfaceOnly" to "true",
+		)
+	)
+
+	additionalProperties.set(
+		mapOf(
+			"useTags" to "true"
+		)
+	)
+
+	typeMappings.set(mapOf("DateTime" to "LocalDateTime"))
+	importMappings.set(mapOf("LocalDateTime" to "java.time.LocalDateTime"))
+}
+
+tasks.compileKotlin {
+	dependsOn("generateApiServer")
+}
+
+kotlin.sourceSets.main {
+	kotlin.srcDirs("src/main/kotlin", "$buildDir/openapi/server-code/src/main")
 }
